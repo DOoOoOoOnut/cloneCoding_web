a448cad8a35a4ad6289c88490f024ec65ca687ce
DOoOoOoOnut <intro2eun@gmail.com>
2024-01-16 16:52:57 +0900

    로그인 부분 완성

--- naver/script/login.js
+++ naver/script/login.js
@@ -10,12 +10,17 @@ console.log(login_title,id_login_container,disposable_login_container,qr_login_c
 // 초기 일회용, qr 내용 숨기기
 disposable_login_container.style.display ='none'
 qr_login_container.style.display ='none'
+// 초기값 id로그인 활성화 시키기(active) 클릭 전
+login_title[0].parentElement.classList.add('active')
 
 // ID로그인 클릭하면 
 login_title[0].addEventListener('click',()=>{
     id_login_container.style.display = 'block'
     qr_login_container.style.display = 'none'
     disposable_login_container.style.display ='none'
+    login_title[0].parentElement.classList.add('active')
+    login_title[1].parentElement.classList.remove('active')
+    login_title[2].parentElement.classList.remove('active')
 })
 
 // 일회용 클릭하면 
@@ -23,6 +28,9 @@ login_title[1].addEventListener('click',()=>{
     id_login_container.style.display = 'none'
     qr_login_container.style.display = 'none'
     disposable_login_container.style.display ='block'
+    login_title[0].parentElement.classList.remove('active')
+    login_title[1].parentElement.classList.add('active')
+    login_title[2].parentElement.classList.remove('active')
 })
 
 // QR코드 클릭하면
@@ -30,5 +38,8 @@ login_title[2].addEventListener('click',()=>{
     id_login_container.style.display = 'none'
     qr_login_container.style.display = 'block'
     disposable_login_container.style.display ='none'
+    login_title[0].parentElement.classList.remove('active')
+    login_title[1].parentElement.classList.remove('active')
+    login_title[2].parentElement.classList.add('active')
 })
 
