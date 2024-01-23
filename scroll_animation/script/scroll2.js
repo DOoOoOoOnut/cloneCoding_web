6e215c5a3e52c46b2ebc9be29dfe1906935b397b
DOoOoOoOnut <intro2eun@gmail.com>
2024-01-23 16:02:20 +0900

    스크롤,캐쉬기록,제이쿼리 응용

--- scroll_animation/script/scroll2.js
+++ scroll_animation/script/scroll2.js
@@ -1,15 +1,19 @@
 const bg = document.querySelectorAll('.bg');
 const index = document.querySelectorAll('.indexBtn')
 const bar = document.querySelectorAll('.bar')
-console.log(bg,index,bar)
+const circle = document.querySelector('.circle')
+console.log(bg,index,bar,circle)
 bar[0].classList.add('active')
 
 // 페이지 이동
 index.forEach((t,i)=>{
-    t.addEventListener('click',()=>{
+    t.addEventListener('click',(e)=>{
+        e.preventDefault()
         window.scrollTo({left:0,top:bg[i].offsetTop})
     })
-    if(window.offsetTop == bg[i].offsetTop){
-        bar[i].classList.add('active')
-    }
+})
+
+window.addEventListener('mousemove',(e)=>{
+    circle.style.left = `${e.clientX}px`
+    circle.style.top = `${e.clientY}px`
 })
